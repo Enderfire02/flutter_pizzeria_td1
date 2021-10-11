3add065cae936044cc79059c655333b3f38cfccb
Cyrus <74969028+Enderfire02@users.noreply.github.com>
2021-10-11 14:53:14 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_pizzeria_td1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_pizzeria_td1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pizzeria_td1/menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.@override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pizzéria',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Notre pizzeria'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  String title;
+  MyHomePage({required this.title, Key? key}) : super(key: key);
+
+  var _menus = [
+    Menu(1, 'Entrées', 'assets/images/menus/entree.png', Colors.lightGreen),
+    Menu(2, 'Pizzas', 'assets/images/menus/pizza.png', Colors.redAccent),
+    Menu(3, 'Desserts', 'assets/images/menus/dessert.png', Colors.brown),
+    Menu(4, 'Boissons', 'assets/images/menus/boisson.png', Colors.lightBlue),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: _menus.length,
+          itemBuilder: (context, index) => _buildRow(_menus[index]),
+          itemExtent: 180,
+        ),
+      ),
+    );
+  }
+
+  _buildRow(Menu menu) {
+    return Container(
+      height: 180,
+      decoration: BoxDecoration(
+        color: menu.color,
+        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+      ),
+      margin: EdgeInsets.all(4.0),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset(
+              menu.image,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Container(
+            height: 50,
+            child: Center(
+              child: Text(
+                menu.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                  fontSize: 28,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
